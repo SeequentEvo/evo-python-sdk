bc2c1e92f5223abfda01c76b37486b80cf8b1a72
Griffin Baxter <74513598+GriffinBaxterSeequent@users.noreply.github.com>
2026-01-22 09:08:06 +1300

    Simplify docs/site structure

--- mkdocs/gen_api_docs.py
+++ mkdocs/gen_api_docs.py
@@ -17,22 +17,24 @@ log = logging.getLogger("mkdocs.gen_api_docs")
 
 def on_startup(command: str, dirty: bool) -> None:
     mkdocs_dir = Path(__file__).parent
-    docs_dir = mkdocs_dir / "docs"
+    docs_packages_dir = mkdocs_dir / "docs" / "packages"
 
     api_clients_file = mkdocs_dir / "api_clients.txt"
     api_clients = [line.strip() for line in api_clients_file.read_text().splitlines() if line.strip()]
     log.info(f"Loaded {len(api_clients)} API clients from {api_clients_file.relative_to(mkdocs_dir)}")
 
-    for old_md in docs_dir.rglob("*.md"):
+    for old_md in docs_packages_dir.rglob("*.md"):
         old_md.unlink()
         log.info(f"Deleted old doc: {old_md.relative_to(mkdocs_dir)}")
 
     for module_path in api_clients:
-        doc_name = "/".join(module_path.split(".")[:-1])  # Exclude the class name
+        _, package, _, _, sub_package, *rest = module_path.split(".")
+        doc_name = f"{package}/{sub_package}" if package == "evo-sdk-common" else package
 
-        doc_path = docs_dir / f"{doc_name}.md"
+        doc_path = docs_packages_dir / f"{doc_name}.md"
         doc_path.parent.mkdir(parents=True, exist_ok=True)
         content = f"::: {module_path}\n"
 
-        doc_path.write_text(content)
+        with doc_path.open("x") as f:
+            f.write(content)
         log.info(f"Generated: {doc_path.relative_to(mkdocs_dir)}")
