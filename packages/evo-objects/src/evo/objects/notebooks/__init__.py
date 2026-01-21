fa597bcdcd1b22a88a1c3630b50a5232255ef1d9
Denis Simo <52428161+denis-simo@users.noreply.github.com>
2026-01-22 09:24:35 +1300

    CPT-1149: basic variogram visualisation, basic mesh without dependencies.

--- packages/evo-objects/src/evo/objects/notebooks/__init__.py
+++ packages/evo-objects/src/evo/objects/notebooks/__init__.py
@@ -1,4 +1,4 @@
-#  Copyright © 2026 Bentley Systems, Incorporated
+#  Copyright © 2025 Bentley Systems, Incorporated
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
@@ -9,28 +9,49 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-"""Notebook visualization tools for geoscience objects.
+"""Ellipsoid and variogram data generation for visualization.
 
-This module provides interactive visualization functions for use in Jupyter notebooks.
-Requires the 'viz' optional dependency: pip install evo-objects[viz]
+This module provides functions to generate mesh/wireframe data for 3D ellipsoids
+and curve data for 2D variogram plots. The output is numpy arrays that can be
+used with any plotting library (Plotly, K3D, matplotlib, etc.).
+
+No external plotting dependencies are required - just numpy.
+
+Example with Plotly:
+    >>> from evo.objects.notebooks import generate_ellipsoid_wireframe
+    >>> import plotly.graph_objects as go
+    >>>
+    >>> data = generate_ellipsoid_wireframe(ranges=(200, 150, 100), rotation=(45, 30, 0))
+    >>> fig = go.Figure(data=[go.Scatter3d(x=data.x, y=data.y, z=data.z, mode="lines")])
+    >>> fig.show()
+
+Example with K3D:
+    >>> from evo.objects.notebooks import generate_ellipsoid_wireframe
+    >>> import k3d
+    >>> import numpy as np
+    >>>
+    >>> data = generate_ellipsoid_wireframe(ranges=(200, 150, 100))
+    >>> mask = ~np.isnan(data.x)
+    >>> vertices = np.column_stack([data.x[mask], data.y[mask], data.z[mask]]).astype(np.float32)
+    >>> plot = k3d.plot()
+    >>> plot += k3d.line(vertices, color=0x0000ff, width=0.5)
+    >>> plot.display()
 """
 
-from .variogram_plot import (
-    plot_ellipsoids_comparison,
-    plot_search_ellipsoid,
-    plot_variogram,
-    plot_variogram_2d,
-    plot_variogram_3d,
-    plot_variogram_ellipsoids,
-    plot_variogram_model,
+from .ellipsoid import (
+    Ellipsoid,
+    EllipsoidData,
+    VariogramCurveData,
+    generate_ellipsoid_mesh,
+    generate_ellipsoid_wireframe,
+    generate_variogram_curves,
 )
 
 __all__ = [
-    "plot_ellipsoids_comparison",
-    "plot_search_ellipsoid",
-    "plot_variogram",
-    "plot_variogram_2d",
-    "plot_variogram_3d",
-    "plot_variogram_ellipsoids",
-    "plot_variogram_model",
+    "Ellipsoid",
+    "EllipsoidData",
+    "VariogramCurveData",
+    "generate_ellipsoid_mesh",
+    "generate_ellipsoid_wireframe",
+    "generate_variogram_curves",
 ]
